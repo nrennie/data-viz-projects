c4d3755a1a9bc14f6720dbaf1e3bfbf5a46f0475
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-06-28 14:58:40 +0100

    add spi table!

--- statistical-performance/script.R
+++ statistical-performance/script.R
@@ -14,12 +14,17 @@ table_data <- raw_data |>
   rename(
     group = region,
     name = country,
-    Income = income,
-    `Overall score` = overall_score
+    Income = income
   ) |> 
-  filter(year == 2024) |> 
-  select(group, name, Income, `Overall score`) |> 
-  arrange(group, `Overall score`)
+  mutate(Income = str_remove(Income, " income")) |> 
+  filter(year %in% 2016:2024) |> 
+  select(group, name, Income, year, overall_score) |> 
+  arrange(year) |> 
+  pivot_wider(names_from = "year", values_from = overall_score) |> 
+  mutate(`2024 score` = `2024`,
+         `Since 2016` = `2024` - `2016`) |> 
+  arrange(group, `2024 score`) |> 
+  select(group, name, Income, `2024 score`, `2016`:`2024`, `Since 2016`)
 
 
 # Write CSV ---------------------------------------------------------------
